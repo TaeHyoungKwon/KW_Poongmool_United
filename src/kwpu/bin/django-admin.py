9877b0caacd84feb84904de8e0a42fa5bed6d392
TaeHyoungKwon <kth5604@gmail.com>
2018-02-28 18:03:27 +0900

    update requirements.txt

--- src/kwpu/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kth/workspace/python/django/KW_Poongmool_United/src/kwpu/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- src/kwpu/lib/python3.5/site-packages/happenings/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = '0.3.3'
-__url__ = 'https://github.com/wreckage/django-happenings'
